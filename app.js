845230b7e7bff2874005b60a7fdc956d0e4e4474
菅野琢也 <tkano@kannotakuyanoMacBook-Air.local>
2020-12-19 19:31:44 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('My name is Takuya Kanno.'));
+app.get('/', (req, res) => res.send('My name is Kanae Kanno.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
